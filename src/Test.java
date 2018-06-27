170d5f8b8c386d045a7b53e1585e1b7c44c76738
erant <sherlord5@gmail.com>
2018-06-27 16:35:06 +0300

    +Start

--- src/Test.java
+++ src/Test.java
@@ -1,13 +1,48 @@
-import javafx.application.Application;
-import javafx.scene.media.Media;
-import javafx.scene.media.MediaPlayer;
-import javafx.stage.Stage;
-import Server.*;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.*;
+import java.util.concurrent.SynchronousQueue;
+import java.util.concurrent.atomic.AtomicInteger;
 
-public class Test{
+public class Test {
+
     public static void main(String[] args) {
-        Server server = new Server(5400,1000,new ServerStrategySolveSearchProblem());
-        server.start();
-        server.stop();
+        Stack tagans_ki=new Stack();
+        List eran1=new ArrayList();
+        eran1.add(5);
+        eran1.add("steven was right");
+        for(Object x : eran1)
+            System.out.println(x);
+        Collections.synchronizedList(eran1);
+        Integer eran=0;
+        Integer steven=100;
+        System.out.println(eran);
+        System.out.println(steven);
+        System.out.println("......");
+        swap(eran,steven);
+        System.out.println("......");
+        System.out.println(eran);
+        System.out.println(steven);
     }
+
+    private static void swap(Integer eran, Integer steven) {
+        System.out.println(eran);
+        System.out.println(steven);
+        Integer tmp=eran;
+        eran=steven;
+        steven=tmp;
+        System.out.println(eran);
+        System.out.println(steven);
+    }
+
+}
+
+class Testt extends Thread{
+    String s;
+
+    public Testt(String s) {
+        this.s = s;
+    }
+    public void inc(int num)
+    {}
 }
